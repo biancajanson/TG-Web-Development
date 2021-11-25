c1785c8c0ae5fa26e89c315aca7d631d371c584f
biancajanson <biancajanson@gmail.com>
2021-11-25 11:56:46 +0100

    UDEMY course

---
+++ UDEMY course/JAVA/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,9 @@
+//LECTURE: Values and Variables
+
+let country = "Netherlands"
+let continent = "Europe"
+let population = 17
+
+console.log(country);
+console.log(continent);
+console.log(population);
