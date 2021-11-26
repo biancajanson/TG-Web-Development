3f55066454fb1561996fb94dd5b39f663e079992
biancajanson <biancajanson@gmail.com>
2021-11-26 10:49:04 +0100

    UDEMY course assignment

---
+++ UDEMY course/JAVASCRIPT/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,37 @@
+// LECTURE: Values and Variables
+/*
+let country = "The Netherlands"
+let continent = "Europe"
+*/
+let population = 17
+
+/*
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// LECTURE: Data Types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+// LECTURE: let, const and var
+language = "Dutch";
+const country = "The Netherlands";
+const continent = "Europe"
+const isIsland = false;
+// isIsland = true;
+
+// LECTURE: Basic Operators
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description); 
