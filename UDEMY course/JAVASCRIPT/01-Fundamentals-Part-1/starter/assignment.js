389820c6f95c1bbd224f8707396a8725be6cb0ea
biancajanson <biancajanson@gmail.com>
2021-11-27 14:10:35 +0100

    UDEMY assignment

--- UDEMY course/JAVASCRIPT/01-Fundamentals-Part-1/starter/assignment.js
+++ UDEMY course/JAVASCRIPT/01-Fundamentals-Part-1/starter/assignment.js
@@ -3,7 +3,7 @@
 let country = "The Netherlands"
 let continent = "Europe"
 */
-let population = 17
+// let population = 17
 
 /*
 console.log(country);
@@ -21,27 +21,38 @@ console.log(typeof country);
 console.log(typeof language);
 */
 // LECTURE: let, const and var
-language = "Dutch";
-const country = "The Netherlands";
-const continent = "Europe"
-const isIsland = false;
+// language = "Dutch";
+// const country = "The Netherlands";
+// const continent = "Europe"
+// const isIsland = false;
 // isIsland = true;
 
 // LECTURE: Basic Operators
-console.log(population / 2);
-population++;
-console.log(population);
-console.log(population > 6);
-console.log(population < 33);
-const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
-console.log(description); 
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+// console.log(description); 
 
-const descriptionNew = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
-console.log(descriptionNew);
+// const descriptionNew = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+// console.log(descriptionNew);
 
 // LECTURE: Type Conversion and Coercion
-console.log('9'- '5'); // voorspelling: 4
-console.log('19' - '13' + '17') // voorspelling: 617
-console.log('19' - '13' + 17) // voorspelling: 23
-console.log('123' < 57); // voorspelling: false
-console.log(5 + 6 + '4' + 9 - 4 - 2) // voorspelling: 1143
+// console.log('9'- '5'); // voorspelling: 4
+// console.log('19' - '13' + '17') // voorspelling: 617
+// console.log('19' - '13' + 17) // voorspelling: 23
+// console.log('123' < 57); // voorspelling: false
+// console.log(5 + 6 + '4' + 9 - 4 - 2) // voorspelling: 1143
+
+// LECTURE: Equality Operators: == vs. ===
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbours === 1) {
+    console.log('Only 1 border!')
+}   else if (numNeighbours > 1) {
+    console.log('More than 1 border')
+}   else {
+    console.log('No borders')
+}
