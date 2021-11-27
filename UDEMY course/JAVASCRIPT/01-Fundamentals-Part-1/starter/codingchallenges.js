4d5ea09b08649d47ab8b3e36f2de2e784bfe4a65
biancajanson <biancajanson@gmail.com>
2021-11-27 14:10:21 +0100

    UDEMY coding challenges

--- UDEMY course/JAVASCRIPT/01-Fundamentals-Part-1/starter/codingchallenges.js
+++ UDEMY course/JAVASCRIPT/01-Fundamentals-Part-1/starter/codingchallenges.js
@@ -1,25 +1,25 @@
-// CODING CHALLENGE 1
-// const massMark = 78;
-// const heightMark = 1.69;
-// const massJohn = 92;
-// const heightMark = 1.95
+// // CODING CHALLENGE 1
+// // const massMark = 78;
+// // const heightMark = 1.69;
+// // const massJohn = 92;
+// // const heightMark = 1.95
 
-const massMark = 95;
-const heightMark = 1.88;
-const massJohn = 85;
-const heightJohn = 1.76;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
 
-const bmiMark = massMark / heightMark **2;
-const bmiJohn = massJohn / heightJohn**2;
-// console.log(bmiMark, bmiJohn);
+// const bmiMark = massMark / heightMark **2;
+// const bmiJohn = massJohn / heightJohn**2;
+// // console.log(bmiMark, bmiJohn);
 
-const markHigherBMI = bmiMark > bmiJohn;
-// console.log(markHigherBMI);
+// const markHigherBMI = bmiMark > bmiJohn;
+// // console.log(markHigherBMI);
 
-// CODING CHALLENGE 2
-if (bmiMark > bmiJohn) {
-    ;
-    console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`)
-}   else {
-    console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`)
-}
+// // CODING CHALLENGE 2
+// if (bmiMark > bmiJohn) {
+//     ;
+//     console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`)
+// }   else {
+//     console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`)
+// }
