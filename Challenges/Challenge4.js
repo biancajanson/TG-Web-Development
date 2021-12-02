52cb0e1c529885847df6853184b29c41e37d6ed0
biancajanson <biancajanson@gmail.com>
2021-12-02 13:12:20 +0100

    Update challenge 4

--- Challenges/Challenge4.js
+++ Challenges/Challenge4.js
@@ -1,11 +1,33 @@
+let firstDate = document.querySelector(".first-date");
+let secondDate = document.querySelector(".second-date");
+const submitBtn = document.querySelector(".btn-submit");
 
-const dateDifference = (firstDate, secondDate) => {
-    let date1 = new Date(firstDate);
-    let date2 = new Date(secondDate);
-    const diffTime = Math.abs(date2 - date1);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  
-    console.log(diffDays + " days");
-  };
-  
-  dateDifference("12/13/2021", "12/16/2021");
+let firstDateValue = null;
+
+firstDate.addEventListener("change", (e) => {
+  firstDateValue = e.target.value;
+  console.log(firstDateValue);
+});
+let secondDateValue = null;
+
+secondDate.addEventListener("change", (e) => {
+  secondDateValue = e.target.value;
+  console.log(secondDateValue);
+});
+
+const dateDifference = (firstInput, secondInput) => {
+  let date1 = new Date(firstInput);
+  let date2 = new Date(secondInput);
+  const diffTime = Math.abs(date2 - date1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  console.log(diffDays + " days");
+
+  let days = diffDays + " days";
+  document.getElementById("show-date").innerText = days;
+  document.getElementById("show-date").textContent = days;
+};
+
+submitBtn.addEventListener("click", (e) => {
+  dateDifference(firstDateValue, secondDateValue);
+});
