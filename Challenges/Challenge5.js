70057dce52462b3de99a91361bad1781ba153536
biancajanson <biancajanson@gmail.com>
2021-12-09 14:12:37 +0100

    challenge 5 - nog niet compleet
    modal is klaar, alleen nog geen styling toegevoegd

--- Challenges/Challenge5.js
+++ Challenges/Challenge5.js
@@ -1,17 +1,61 @@
 const cat = document.getElementById("catButton");
+const dog = document.getElementById("dogButton");
+const parrot = document.getElementById("parrotButton");
 const modalContainer = document.querySelector(".modalContainer");
 
 cat.onclick = () => {
-    generateModal() 
+    generateModal1() 
 }
 
-function generateModal() {
+function generateModal1() {
     const modal = document.createElement('div');
     modal.classList.add("modal");
     modalContainer.appendChild(modal);
     const modalImage = document.createElement('img');
     modalImage.classList.add('modalImage');
     modalImage.src = 'cat.jpg';
+    modal.appendChild(modalImage);
+    const modalText = document.createElement('p');
+    modalText.classList.add('modalText');
+    modal.appendChild(modalText);
+    modalText.innerText = 'test';
+    modalContainer.onclick = () => {
+        modalContainer.innerHTML = ""
+    }
+} 
+
+dog.onclick = () => {
+    generateModal2() 
+}
+
+function generateModal2() {
+    const modal = document.createElement('div');
+    modal.classList.add("modal");
+    modalContainer.appendChild(modal);
+    const modalImage = document.createElement('img');
+    modalImage.classList.add('modalImage');
+    modalImage.src = 'dog.jpg';
+    modal.appendChild(modalImage);
+    const modalText = document.createElement('p');
+    modalText.classList.add('modalText');
+    modal.appendChild(modalText);
+    modalText.innerText = 'test';
+    modalContainer.onclick = () => {
+        modalContainer.innerHTML = ""
+    }
+} 
+
+parrot.onclick = () => {
+    generateModal3() 
+}
+
+function generateModal3() {
+    const modal = document.createElement('div');
+    modal.classList.add("modal");
+    modalContainer.appendChild(modal);
+    const modalImage = document.createElement('img');
+    modalImage.classList.add('modalImage');
+    modalImage.src = 'parrot.jpg';
     modal.appendChild(modalImage);
     const modalText = document.createElement('p');
     modalText.classList.add('modalText');
