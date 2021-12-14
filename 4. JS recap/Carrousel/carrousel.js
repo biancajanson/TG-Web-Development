3803a72c4fb38c9f753df35c3058ced9e9f475d8
biancajanson <biancajanson@gmail.com>
2021-12-14 09:32:09 +0100

    Update carrousel.js

--- 4. JS recap/Carrousel/carrousel.js
+++ 4. JS recap/Carrousel/carrousel.js
@@ -6,3 +6,7 @@ const dotsNav = document.querySelector('.carrousel__nav');
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
+
+slides[0].style.left = 0;
+slides[1].style.left = slideWidth + 'px';
+slides[2].style.left = slideWidth * 2 + 'px';
