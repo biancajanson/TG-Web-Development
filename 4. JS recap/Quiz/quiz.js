e24fd556464dc81f94b0db9afdeddcc1bb881c15
biancajanson <biancajanson@gmail.com>
2021-12-15 09:59:56 +0100

    Update quiz.js

--- 4. JS recap/Quiz/quiz.js
+++ 4. JS recap/Quiz/quiz.js
@@ -1,9 +1,7 @@
 const start = document.querySelector('.start-button');
 const container = document.querySelector('.container');
-const bottomButtons = document.createElement('div');
-const prevButton = document.createElement('button');
-const nextButton = document.createElement('button');
-
+const prev = document.querySelector('.prev-button');
+const next = document.querySelector('.next-button');
 
 // als je op start klikt, opent er een div met math problems
 start.onclick = () => {
@@ -60,9 +58,12 @@ function beginQuiz() {
     antwoorden.appendChild(antwoord5);
     antwoord5.innerText = '5';
 
+    const bottomButtons = document.createElement('div');
     bottomButtons.classList.add('bottom-buttons');
     quizVragen.appendChild(bottomButtons);
     
+    const prevButton = document.createElement('button');
+    const nextButton = document.createElement('button');
     prevButton.classList.add('prev-button');
     nextButton.classList.add('next-button');
     bottomButtons.appendChild(prevButton);
@@ -70,12 +71,33 @@ function beginQuiz() {
     prevButton.innerText = 'Vorige';
     nextButton.innerText = 'Volgende';
 }
-const prev = document.querySelector('.prev-button');
-const next = document.querySelector('.next-button');
-// bottomButtons.onclick = () => {
-//     nextQuestion()
-// }
 
-// function nextQuestion() {
-
-// }
+// vragen en antwoorden
+function questions() {
+    const allQuestions = [{
+        question: "what is 24 + 42",
+        options: ["45", "66", "82", "69", "68"],
+        answer: 1,
+    }, {
+        question: "what is 5 * 4",
+        options: ["9", "30", "20", "40", "1"],
+        answer: 2,
+    }, {
+        question: "what is 25 - 11",
+        options: ["14", "12", "15", "10", "13"],
+        answer: 0,
+    }, {
+        question: "what is 25 / 5",
+        options: ["5", "20", "30", "125", "10"],
+        answer: 0,
+    }, {
+        question: "what is 1/2 of 6",
+        options: ["2", "1", "9", "12", "3"],
+        answer: 4
+    }, {
+        question: "what is 56 + 11",
+        options: ["102", "37", "44", "67", "50"],
+        answer: 3
+    }];
+    
+}
