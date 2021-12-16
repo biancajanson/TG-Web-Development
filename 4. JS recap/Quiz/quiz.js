d8fad7791d9fb83ab94ca0214d6a46cc8db726c3
biancajanson <biancajanson@gmail.com>
2021-12-16 14:33:29 +0100

    Update quiz

--- 4. JS recap/Quiz/quiz.js
+++ 4. JS recap/Quiz/quiz.js
@@ -3,6 +3,34 @@ const container = document.querySelector('.container');
 const prev = document.querySelector('.prev-button');
 const next = document.querySelector('.next-button');
 
+const questions = [
+    {
+        question: "what is 24 + 42",
+        options: ["45", "66", "82", "69", "68"],
+        answer: 1,
+    }, {
+        question: "what is 5 * 4",
+        options: ["9", "30", "20", "40", "1"],
+        answer: 2,
+    }, {
+        question: "what is 25 - 11",
+        options: ["14", "12", "15", "10", "13"],
+        answer: 0,
+    }, {
+        question: "what is 25 / 5",
+        options: ["5", "20", "30", "125", "10"],
+        answer: 0,
+    }, {
+        question: "what is 1/2 of 6",
+        options: ["2", "1", "9", "12", "3"],
+        answer: 4
+    }, {
+        question: "what is 56 + 11",
+        options: ["102", "37", "44", "67", "50"],
+        answer: 3
+    }
+];
+
 // als je op start klikt, opent er een div met math problems
 start.onclick = () => {
     beginQuiz()
@@ -28,34 +56,34 @@ function beginQuiz() {
     vragen.classList.add('quiz-vraag');
     quizVragen.appendChild(vragen);
     
-    const antwoorden = document.createElement('div');
+    const antwoorden = document.createElement('ol');
     antwoorden.classList.add('quiz-antwoorden');
     quizVragen.appendChild(antwoorden);
 
-    const antwoord1 = document.createElement('p');
-    antwoord1.classList.add('quiz-antwoord1');
-    antwoorden.appendChild(antwoord1);
-    antwoord1.innerText = '1';
+    // const antwoord1 = document.createElement('p');
+    // antwoord1.classList.add('quiz-antwoord1');
+    // antwoorden.appendChild(antwoord1);
+    // antwoord1.innerText = '1';
 
-    const antwoord2 = document.createElement('p');
-    antwoord2.classList.add('quiz-antwoord2');
-    antwoorden.appendChild(antwoord2);
-    antwoord2.innerText = '2';
+    // const antwoord2 = document.createElement('p');
+    // antwoord2.classList.add('quiz-antwoord2');
+    // antwoorden.appendChild(antwoord2);
+    // antwoord2.innerText = '2';
 
-    const antwoord3 = document.createElement('p');
-    antwoord3.classList.add('quiz-antwoord3');
-    antwoorden.appendChild(antwoord3);
-    antwoord3.innerText = '3';
+    // const antwoord3 = document.createElement('p');
+    // antwoord3.classList.add('quiz-antwoord3');
+    // antwoorden.appendChild(antwoord3);
+    // antwoord3.innerText = '3';
 
-    const antwoord4 = document.createElement('p');
-    antwoord4.classList.add('quiz-antwoord4');
-    antwoorden.appendChild(antwoord4);
-    antwoord4.innerText = '4';
+    // const antwoord4 = document.createElement('p');
+    // antwoord4.classList.add('quiz-antwoord4');
+    // antwoorden.appendChild(antwoord4);
+    // antwoord4.innerText = '4';
 
-    const antwoord5 = document.createElement('p');
-    antwoord5.classList.add('quiz-antwoord5');
-    antwoorden.appendChild(antwoord5);
-    antwoord5.innerText = '5';
+    // const antwoord5 = document.createElement('p');
+    // antwoord5.classList.add('quiz-antwoord5');
+    // antwoorden.appendChild(antwoord5);
+    // antwoord5.innerText = '5';
 
     const bottomButtons = document.createElement('div');
     bottomButtons.classList.add('bottom-buttons');
