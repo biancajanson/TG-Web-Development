d4be022072a7c9205c7ef1659f7ab4caf24a75d0
biancajanson <biancajanson@gmail.com>
2021-12-17 10:11:29 +0100

    Update quiz.js

--- 4. JS recap/Quiz/quiz.js
+++ 4. JS recap/Quiz/quiz.js
@@ -3,41 +3,7 @@ const container = document.querySelector('.container');
 const prev = document.querySelector('.prev-button');
 const next = document.querySelector('.next-button');
 
-// const questionContainer = [
-//     {
-//         question: "what is 24 + 42",
-//         options: ["45", "66", "82", "69", "68"],
-//         answer: 1,
-//     }, {
-//         question: "what is 5 * 4",
-//         options: ["9", "30", "20", "40", "1"],
-//         answer: 2,
-//     }, {
-//         question: "what is 25 - 11",
-//         options: ["14", "12", "15", "10", "13"],
-//         answer: 0,
-//     }, {
-//         question: "what is 25 / 5",
-//         options: ["5", "20", "30", "125", "10"],
-//         answer: 0,
-//     }, {
-//         question: "what is 1/2 of 6",
-//         options: ["2", "1", "9", "12", "3"],
-//         answer: 4
-//     }, {
-//         question: "what is 56 + 11",
-//         options: ["102", "37", "44", "67", "50"],
-//         answer: 3
-//     }
-// ];
-
-// als je op start klikt, opent er een div met math problems
-start.onclick = () => {
-    startQuiz()
-}
-
-function startQuiz() {
-    const questionContainer = [
+const questionContainer = [
     {
         question: "what is 24 + 42",
         options: ["45", "66", "82", "69", "68"],
@@ -63,7 +29,41 @@ function startQuiz() {
         options: ["102", "37", "44", "67", "50"],
         answer: 3
     }
-    ];
+];
+
+// als je op start klikt, opent er een div met math problems
+start.onclick = () => {
+    startQuiz()
+}
+
+function startQuiz() {
+    // const questionContainer = [
+    // {
+    //     question: "what is 24 + 42",
+    //     options: ["45", "66", "82", "69", "68"],
+    //     answer: 1,
+    // }, {
+    //     question: "what is 5 * 4",
+    //     options: ["9", "30", "20", "40", "1"],
+    //     answer: 2,
+    // }, {
+    //     question: "what is 25 - 11",
+    //     options: ["14", "12", "15", "10", "13"],
+    //     answer: 0,
+    // }, {
+    //     question: "what is 25 / 5",
+    //     options: ["5", "20", "30", "125", "10"],
+    //     answer: 0,
+    // }, {
+    //     question: "what is 1/2 of 6",
+    //     options: ["2", "1", "9", "12", "3"],
+    //     answer: 4
+    // }, {
+    //     question: "what is 56 + 11",
+    //     options: ["102", "37", "44", "67", "50"],
+    //     answer: 3
+    // }
+    // ];
 
     const quizContainer = document.createElement('div');
     quizContainer.classList.add('container-questions');
@@ -83,13 +83,7 @@ function startQuiz() {
     const questions = document.createElement('div');
     questions.classList.add('quiz-question');
     quizContainer.appendChild(questions);
-    // questions.innerText = 'hier komen de vragen';
-    const question = [];
-        for (let i=0; i<questionContainer.question; i++) {
-            return questionContainer.question[i]
-        }
-
-    // console.log(question)
+    questions.innerText = 'hier komen de vragen';
 
 
 
