13dabc2eee3d61ec6b78e97d6470ce137ccae393
biancajanson <biancajanson@gmail.com>
2022-02-16 10:14:20 +0100

    Java

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         System.out.println("Hello World");
-    }
+    }*/
+
+    int x = 5;
+    int y = 3;
+
+    /* public static void main(String[] args) {
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.y);
+    } */
 
     // Single line comment
     /* Multi line
