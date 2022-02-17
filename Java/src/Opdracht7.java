1a8ac4295974d7fb6169c12cb1875f6ed59eaa77
biancajanson <biancajanson@gmail.com>
2022-02-17 10:01:53 +0100

    Opdracht 7a

---
+++ Java/src/Opdracht7.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Opdracht7 {
+    public static void main(String[] args) {
+        String[] eastcars = {"Kia", "Honda"};
+        String[] cars = {"Kia", "Volkswagen", "Renault", "Honda", "BMW"};
+        ArrayList<String> westcars = new ArrayList<>();
+
+        // Opdracht a
+        for (int i = 0; i < 3; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
