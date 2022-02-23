15a62bd600c9206dd5a8c1b783b9351cade43348
biancajanson <biancajanson@gmail.com>
2022-02-23 11:26:01 +0100

    udemy float and double

---
+++ UDEMY course/JAVA/ByteShortIntLong/src/FloatDouble.java
@@ -0,0 +1,22 @@
+public class FloatDouble {
+
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3; // geen decimalen, rest wordt weg gelaten
+        float myFloatValue = 5f / 3f; // 7 decimalen
+        double myDoubleValue = 5d / 3d; // 16 decimalen
+        System.out.println("MyIntvalue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+    }
+
+}
