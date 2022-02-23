b43d3b7ddc6cb9c74fceecdca4938ec17055e9ed
biancajanson <biancajanson@gmail.com>
2022-02-23 15:23:24 +0100

    char and boolean

--- UDEMY course/JAVA/ByteShortIntLong/src/CharAndBoolean.java
+++ UDEMY course/JAVA/ByteShortIntLong/src/CharAndBoolean.java
@@ -1,6 +1,20 @@
 public class CharAndBoolean {
 
     public static void main(String[] args) {
+
+        char myChar = 'D'; // maar 1 karakter
+        // wordt gebruikt om bijv laatste aangeslagen letter op te slaan in een game
+        char myUnicodeChar = '\u0044';
+        // unicode-table.com
+        char myCopyright = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyright);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
         
     }
 }
